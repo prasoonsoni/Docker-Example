e6f05252ac273c7a32a4963a4946fe4201bd91b5
Prasoon Soni <prasoonsoni03@gmail.com>
2023-01-30 18:59:28 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res)=>{
+    res.send("<h1>Working Fine :)</h1>")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`)
+})
